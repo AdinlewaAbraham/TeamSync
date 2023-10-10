7bcc8c4a3330cab02f917dbd01e2513251f78fec
AdinlewaAbraham <abrahamadinlewa@gmail.com>
2023-10-10 21:31:01 +0100

    timeline basics

--- frontend/app/project/[projectId]/tasks/timeline/page.tsx
+++ frontend/app/project/[projectId]/tasks/timeline/page.tsx
@@ -6,6 +6,13 @@ const page = () => {
       <nav className="flex justify-between items-center py-2 text-sm  border-t border-border-default">
         <div> add task</div>
       </nav>
+      <h1>what to do</h1>
+      <ol>
+        <li>initaillty render three or four months </li>
+        <li>add horizontal infinity scroll</li>
+        <li>dont make custom components for diff time views "months, weeks, years" </li>
+        <li>render all tasks in col from the header down like having long veritical bars  </li>
+      </ol>
       <div className=" border-t border-border-default"></div>
     </div>
   );
--- frontend/components/project/timeline/MonthTimeline.tsx
+++ frontend/components/project/timeline/MonthTimeline.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MonthTimeline = () => {
   return (
-    <div>MonthTimeline</div>
+    <div>
+      
+    </div>
   )
 }
 
