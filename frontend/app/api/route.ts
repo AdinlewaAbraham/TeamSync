0b401cc6a738a4908a255c872eec65a490bafe99
abraham <abrahamadinlewa@gmail.com>
2024-02-22 20:55:59 +0100

    at 11

--- frontend/app/api/route.ts
+++ frontend/app/api/route.ts
@@ -4,5 +4,5 @@ const dataSource = "http://localhost:5001/";
 export async function GET(req: NextRequest, res: NextResponse) {
   console.log("you might me");
   const response = await fetch(dataSource, { method: "GET" });
-  return NextResponse.json(await response.json(), { status: response.status });
+  return NextResponse.json({ mess: "sup my niggs" });
 }
