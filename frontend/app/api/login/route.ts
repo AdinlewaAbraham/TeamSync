a46e9483cf480014216873997608e6b09f853c0c
abraham <abrahamadinlewa@gmail.com>
2024-02-22 20:50:33 +0100

    at 10

--- frontend/app/api/login/route.ts
+++ frontend/app/api/login/route.ts
@@ -2,5 +2,5 @@ import { NextRequest, NextResponse } from "next/server";
 
 const dataSource = "http://localhost:5001/auth/google";
 export async function GET(req: NextRequest, res: NextResponse) {
-  NextResponse.json({ mess: "sup my niggs" });
+  return NextResponse.json({ mess: "sup my niggs" });
 }
